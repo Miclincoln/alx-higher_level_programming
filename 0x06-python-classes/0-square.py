528a85ab7bd8b7bea0d0c56eb8c0da794a5d6793
Kayode Gbenga Michael <Kayodemichael1@gmail.com>
2023-04-14 08:03:45 +0100

    Add documentation

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 """
-This is a simple "Square" module.
+This is the "Square" module.
 
 This module provides a simple Square class.
 """
 
 class Square:
-"""An empty Square class"""
+"""A simple Square class"""
     pass
