d41111a6a710e10ffabdb350b0623eeacbe95b4c
Kayode Gbenga Michael <Kayodemichael1@gmail.com>
2023-04-19 09:29:26 +0100

    Adds documentation

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,3 +1,19 @@
 #!/usr/bin/python3
+
+""" A function module. """
+
+
 def is_same_class(obj, a_class):
+
+    """
+    Definition of class "is_same_class"
+
+    Args:
+       obj: An object
+       a_class: A class.
+
+    Return:
+         Return True if "obj" is an instance of "a_class".
+    """
+
     return type(obj) == a_class
