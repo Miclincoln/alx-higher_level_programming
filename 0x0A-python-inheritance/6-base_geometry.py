c44ba11edd2edf4fce80211e0e32bd5a7690973b
Kayode Gbenga Michael <Kayodemichael1@gmail.com>
2023-04-19 09:58:33 +0100

    Adds documentation

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,18 @@
+#!usr/bin/python3
+
+""" A class module. """
+
+
+class BaseGeometry:
+
+    """ Definition of an empty class ```BaseGeometry`` """
+
+    def area(self):
+        """
+        definition of public instance method ``area``.
+
+        Raises:
+              Exception: area() is not implemented.
+        """
+
+        raise Exception("area() is not implemented")
