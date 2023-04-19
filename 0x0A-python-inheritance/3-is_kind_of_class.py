4ea75042c9671e2fc5e83e6e446c12d9a8fa2dec
Kayode Gbenga Michael <Kayodemichael1@gmail.com>
2023-04-19 09:37:13 +0100

    Adds documentation

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -1,3 +1,19 @@
 #!/usr/bin/python3
+
+""" A function module"""
+
+
 def is_kind_of_class(obj, a_class):
+
+    """
+    Definition of function ``is_kind_of_class``.
+
+    args:
+        obj: First parameter, an object
+        a_class: Second parameter, a class.
+
+    return:
+        Returns True if ``obj`` is an instance of ``a_class`` or inherit from.
+    """
+
     return isinstance(obj, a_class)
