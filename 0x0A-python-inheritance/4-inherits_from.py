b280b894c3230ea19bf4aa373a5596fc1d8486f2
Kayode Gbenga Michael <Kayodemichael1@gmail.com>
2023-04-19 09:43:36 +0100

    Adds documentation

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,5 +1,21 @@
 #!/usr/bin/python3
+
+""" A function module """
+
+
 def inherits_from(obj, a_class):
+
+    """
+    Definition of the function ``inherits_from``.
+
+    args:
+       obj: First parameter, an object
+       a_class: Second parameter, a class.
+
+    Return:
+          True if ``obj`` is an instance of ``a_class`` else False
+    """
+
     if (type(obj) != a_class):
         return issubclass(type(obj), a_class)
     return False
