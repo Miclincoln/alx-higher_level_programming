506d20d96158fb5d8cb79e81498d41570e5e7313
Kayode Gbenga Michael <Kayodemichael1@gmail.com>
2023-04-19 09:09:20 +0100

    Adds documentation

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,3 +1,18 @@
+#!/usr/bin/python3
+
+""" A class module of inheritance"""
+
+
 class MyList(list):
+    """
+    Definition of class "MyList".
+
+    Inheritance:
+        Inherits from "List".
+    """
+
     def print_sorted(self):
+
+        """ Prints the list in sorted ascending order."""
+
         print(sorted(self))
