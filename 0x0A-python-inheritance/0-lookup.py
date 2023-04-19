a5c1154f80159a9489b0c7f24fad973c2cce9ada
Kayode Gbenga Michael <Kayodemichael1@gmail.com>
2023-04-19 09:23:35 +0100

    Adds documentation

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,3 +1,15 @@
 #!/usr/bin/python3
+
+""" A function module."""
+
+
 def lookup(obj):
+
+    """
+    definition of the fucntion "lookup".
+
+    Return:
+         Attributes and methods of "obj".
+    """
+
     return dir(obj)
