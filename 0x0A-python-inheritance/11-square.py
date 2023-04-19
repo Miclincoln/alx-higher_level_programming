4456120256da5db8cb96ccb67c9c905a839897c9
Kayode Gbenga Michael <Kayodemichael1@gmail.com>
2023-04-19 11:55:03 +0100

    Square #2

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -1,13 +1,20 @@
 #!/usr/bin/python3
+
 """ A class module """
 
 Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
-    """ Definition of calss ``Square``"""
+    """ 
+    Definition of calss ``Square``.
+
+    args:
+        Rectangle: Inherited class
+    """
     def __init__(self, size):
         """ Initializing class ``Square``"""
+
         super().__init__(size, size)
         self.__size = size
 
