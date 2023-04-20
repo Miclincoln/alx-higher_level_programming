a4c574c2382fe19cb71bb26ae53a10c14f80658d
Kayode Gbenga Michael <Kayodemichael1@gmail.com>
2023-04-20 14:28:35 +0100

    task 0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -7,9 +7,10 @@ def read_file(filename=""):
     Definition of the ``read_file`` function.
 
     args:
-        filename (string): First parameter, accepts a string.
+        filename (string): First parameter, accepts a string .
     prints:
         Reads the text file to stdout.
     """
-    with open('my_file_0.txt', encoding='utf-8')as file1:
-            print(file1.read(), end="")
+    with open(filename, encoding="utf-8") as file1:
+        for text in file1:
+            print(file1, end="")
