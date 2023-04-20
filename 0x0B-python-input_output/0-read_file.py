7493176f6aabe99f04a6030bcde2520ea3d9d725
Kayode Gbenga Michael <Kayodemichael1@gmail.com>
2023-04-20 14:19:11 +0100

    Removes the file permission

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -11,6 +11,5 @@ def read_file(filename=""):
     prints:
         Reads the text file to stdout.
     """
-    with open('my_file_0.txt', mode='r', encoding='utf-8')as file1:
-        for text in file1:
-            print(text, end="")
+    with open('my_file_0.txt', encoding='utf-8')as file1:
+            print(file1.read(), end="")
