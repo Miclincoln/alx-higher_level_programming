4512acde7fb69c4fdc5c49da6124591ebe387972
Kayode Gbenga Michael <Kayodemichael1@gmail.com>
2023-04-20 14:11:19 +0100

    Write a file

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" A function module. """
+
+
+def read_file(filename=""):
+    """
+    Definition of the ``read_file`` function.
+
+    args:
+        filename (string): First parameter, accepts a string.
+    prints:
+        Reads the text file to stdout.
+    """
+    with open('my_file_0.txt', mode='r', encoding='utf-8')as file1:
+        print(file1.read(), end="")
