5b85c8816f29da70471e9fd5449a7bd922fb9e2d
Kayode Gbenga Michael <Kayodemichael1@gmail.com>
2023-04-21 10:35:16 +0100

    The JSON string

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+import json
+""" A module that return JSON representation of a string. """
+
+
+def to_json_string(my_obj):
+    """
+    Definition of the function ``to_json_string``.
+
+    args:
+       my_obj (string): Object whose json representation is to be returned.
+
+    var:
+       json_data: Stores json representation of an object ``my_obj``.
+
+    return:
+       json_data
+    """
+    json_data = json.dumps(my_obj)
+    return json_data
